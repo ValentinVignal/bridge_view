fe5f437fa88065a9555e93adf7eb5a7d2e9281a2
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-12-10 23:19:55 +0800

    Create rust server and flutter client

---
+++ client/android/app/src/main/kotlin/com/example/bridge_view_client/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bridge_view_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ server/target/debug/deps/bridge_view_server-b4996a31d63dd3d1.d
@@ -0,0 +1,5 @@
+/Users/valentinvignal/Projects/bridge_view/server/target/debug/deps/bridge_view_server-b4996a31d63dd3d1.d: src/main.rs
+
+/Users/valentinvignal/Projects/bridge_view/server/target/debug/deps/libbridge_view_server-b4996a31d63dd3d1.rmeta: src/main.rs
+
+src/main.rs:
