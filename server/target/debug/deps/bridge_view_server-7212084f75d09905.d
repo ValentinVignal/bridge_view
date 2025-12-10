81a87a432268604b4ebdec5b3e5eab9d6c176b14
ValentinVignal <valentin.vignal.dev@outlook.fr>
2025-12-10 23:25:28 +0800

    Remove target

--- server/target/debug/deps/bridge_view_server-7212084f75d09905.d
+++
@@ -1,5 +0,0 @@
-/Users/valentinvignal/Projects/bridge_view/server/target/debug/deps/bridge_view_server-7212084f75d09905.d: src/main.rs
-
-/Users/valentinvignal/Projects/bridge_view/server/target/debug/deps/libbridge_view_server-7212084f75d09905.rmeta: src/main.rs
-
-src/main.rs:
--- server/target/debug/deps/bridge_view_server-b4996a31d63dd3d1.d
+++
@@ -1,5 +0,0 @@
-/Users/valentinvignal/Projects/bridge_view/server/target/debug/deps/bridge_view_server-b4996a31d63dd3d1.d: src/main.rs
-
-/Users/valentinvignal/Projects/bridge_view/server/target/debug/deps/libbridge_view_server-b4996a31d63dd3d1.rmeta: src/main.rs
-
-src/main.rs:
